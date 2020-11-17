48a133d965011ee0a0e9680eaf1e2fce1d5975aa
Bakhos Rachwan <bakhos.rachwan@gmail.com>
2020-11-17 22:59:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Your code goes here
 document.addEventListener("DOMContentLoaded", function() {
-  document.querySelector("#text").innerHTML = 'This is really cool';
+  document.querySelector("#text").innerHTML = 'This is really cool!';
   console.log("The DOM has loaded");
 });
  
